db3f3467d673e15ebb89e4049ecdcc26514dc23a
Jelena <jelena.durovic@powerhrg.com>
2021-09-10 09:52:05 -0400

    commit tree d7dd8ff5a65c98ae4aaf160867d04e4453f730ab

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,13 @@ class Post < ApplicationRecord
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-    validates :title, exclusion: {in: ["True Facts"]}
+    #validates :title, exclusion: {in: ["True Facts"]}
+    validate :clickbaity?
+
+    def clickbaity?
+        baits = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+        if baits.none? {|bait| title&.include? bait}
+        errors.add(:title, "Title is not clickbaity enough")
+    end 
+    end 
 end
